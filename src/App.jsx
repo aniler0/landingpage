bcfe59597bd6fa39ff865f87388310ade0942df6
aniler0 <anil.er@hotmail.com>
2021-08-24 20:26:29 +0300

    :tada: first commit, there is not new thing

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./Pages/Home/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Home />
+      {/*About*/}
+      {/*Plan*/}
+      {/*Locations */}
+      {/*Customers */}
+      {/*Footer */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div>about</div>
+      <div className="navbar">navbar</div>
+    </div>
+  );
+};
+
+export default Home;
