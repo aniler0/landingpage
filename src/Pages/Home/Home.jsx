13c247c89f0864dd313397fd2d9728bbdc944665
aniler0 <anil.er@hotmail.com>
2021-08-24 20:45:15 +0300

    :construction: created Home Page and Navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
-      <div>about</div>
-      <div className="navbar">navbar</div>
+      <Navbar />
+      {/*HomeCenter*/}
+      {/*Statistics*/}
     </div>
   );
 };
