e3e8640ba5778a34b1933e09e7c2480542a5b3fc
aniler0 <anil.er@hotmail.com>
2021-08-24 22:12:46 +0300

    :construction: created Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import logo from "assets/images/logo.svg";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__Container">
+        <div className="navbar__Left">
+          <div className="banner__Logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <h1>
+            Lasles<span>VPN</span>
+          </h1>
+        </div>
+        <div className="navbar__Center">
+          <a>About</a>
+          <a>Features</a>
+          <a>Pricing</a>
+          <a>Testimonials</a>
+          <a>Help</a>
+        </div>
+        <div className="navbar__Right">
+          <button className="signin">Sign In</button>
+          <button className="signup">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
